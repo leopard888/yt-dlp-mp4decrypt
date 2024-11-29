ae1a88a34db855133974e62948330f84bde10028
aarubui <aarubui@users.noreply.github.com>
2024-11-30 10:27:42 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+This [yt-dlp](https://github.com/yt-dlp/yt-dlp) plugin integrates `mp4decrypt` and `pywidevine` to streamline downloading and decryption.
+
+## Prerequisites
+
+* The `mp4decrypt` executable (part of [Bento4](https://www.bento4.com/)) in your system's PATH
+* A CDM in .wvd format
+
+## Installation
+
+You can install this package with pip:
+```
+python3 -m pip install -U https://github.com/aarubui/yt-dlp-mp4decrypt/archive/master.zip
+```
+
+## Usage
+
+Use `--use-postprocessor` to activate the plugin. This can be added to configuration files without effects for unencrypted videos.
+
+```shell
+yt-dlp --allow-unplayable-formats --use-postprocessor Mp4Decrypt:devicepath=<path_to_wvd_file> <video_url>
+```
+
+When requesting merging of multiple formats, a warning will appear saying the formats won't be merged. This plugin forces the merge to take place.
+
+## Supported extractors
+
+Sites supported by `yt-dlp` where unplayable formats are returned and the license URL is provided in the MPD (e.g. Brightcove) will work out of the box with this plugin. Extractors which give the `This video is DRM protected` error even with `--allow-unplayable-formats` won't work.
+
+## Extending support
+
+Add support for a site by writing your own [plugin](https://github.com/yt-dlp/yt-dlp#plugins). The following extra fields are supported in the info dict:
+
+- `_cenc_key` (decryption key in `kid:key` format)
+- `_license_url` (URL to license server)
+- `_license_callback` (function which takes the `challenge` data and returns the license server response)
+
+Use `_license_callback` when communication with the license server needs customisation (e.g. server request needs to be base64-encoded).
+
+### Example
+
+```python
+from yt_dlp.extractor.common import InfoExtractor
+
+
+class YourExtractorIE(InfoExtractor):
+    _VALID_URL = r'https?://(?:www\.)?yourextractor\.com/watch/(?P<id>[0-9]+)'
+
+    def _real_extract(self, url):
+        video_id = self._match_id(url)
+        mpd_url = 'https://yourextractor.com/' + video_id + '.mpd'
+
+        return {
+            'id': video_id,
+            'title': video_id,
+            'formats': self._extract_mpd_formats(mpd_url, video_id),
+            '_license_url': 'https://cwip-shaka-proxy.appspot.com/no_auth',
+        }
+```
---
+++ yt_dlp_plugins/postprocessor/mp4decrypt.py
@@ -0,0 +1,162 @@
+from os import rename, replace
+from pywidevine.cdm import Cdm
+from pywidevine.device import Device
+from pywidevine.pssh import PSSH
+from re import sub
+from yt_dlp.networking.common import Request
+from yt_dlp.postprocessor import FFmpegMergerPP
+from yt_dlp.postprocessor.common import PostProcessor
+from yt_dlp.utils import Popen, PostProcessingError
+import subprocess
+
+class Mp4DecryptPP(PostProcessor):
+    _kwargs = {}
+    _pssh = {}
+    _license_urls = {}
+    _keys = {}
+
+    _WVXPATH = './/{*}ContentProtection[@schemeIdUri=\'' + PSSH.SystemId.Widevine.urn + '\']'
+
+    def __init__(self, downloader=None, **kwargs):
+        PostProcessor.__init__(self, downloader)
+        self._sniff_mpds(downloader)
+        self._kwargs = kwargs
+
+    def _sniff_mpds(self, downloader):
+        oldextmethod = downloader.add_info_extractor
+
+        def newextmethod(ie):
+            oldmpdmethod = ie._parse_mpd_periods
+
+            def newmpdmethod(mpd_doc, *args, **kwargs):
+                if not (element := mpd_doc.find(self._WVXPATH)) == None:
+                    mpd_url = kwargs.get('mpd_url') or args[2]
+                    self._pssh[mpd_url] = element.findtext('./{*}pssh')
+                    self._license_urls[mpd_url] = element.get('{urn:brightcove:2015}licenseAcquisitionUrl')
+                elif not mpd_doc.find('.//{*}ContentProtection') == None:
+                    # remove playready, etc.
+                    return []
+
+                return oldmpdmethod(mpd_doc, *args, **kwargs)
+
+            ie._parse_mpd_periods = newmpdmethod
+            oldextmethod(ie)
+
+        downloader.add_info_extractor = newextmethod
+
+    def run(self, info):
+        encrypted = []
+
+        if 'requested_formats' in info:
+            for part in info['requested_formats']:
+                if self._is_encrypted(part):
+                    encrypted.append(part)
+        elif info['__real_download'] and self._is_encrypted(info):
+            encrypted.append(info)
+
+        if encrypted:
+            decrypted = True
+
+            for part in encrypted:
+                if part['protocol'] == 'http_dash_segments' and (keys := self._get_keys(info, part)):
+                    self._decrypt_part(keys, part['filepath'])
+                else:
+                    self.to_screen('Cannot decrypt ' + part['format_id'])
+                    decrypted = False
+
+            if decrypted and '+' in info['format_id']:
+                info['__files_to_merge'] = [part['filepath'] for part in info['requested_formats']]
+                info = self._downloader.run_pp(FFmpegMergerPP(), info)
+
+        return [], info
+
+    def _is_encrypted(self, info):
+        return 'filepath' in info and info.get('has_drm')
+
+    def _get_keys(self, info, part):
+        if key := info.get('_cenc_key'):
+            return ('--key', key)
+
+        mpd_url = part['manifest_url']
+        pssh = self._pssh.get(mpd_url)
+        license_callback = info.get('_license_callback')
+        license_url = info.get('_license_url', self._license_urls.get(mpd_url))
+
+        if not license_callback and license_url:
+            def license_callback(challenge):
+                self.to_screen(f'Fetching keys from {license_url}')
+                return self._downloader.urlopen(Request(license_url, data=challenge)).read()
+
+        if not pssh:
+            pssh = self._pssh_from_init(part)
+
+        if pssh and license_callback:
+            return self._fetch_keys(info, pssh, license_callback)
+
+        return ()
+
+    def _pssh_from_init(self, part):
+        def find_wv_pssh_offsets(raw):
+            offset = 0
+
+            while True:
+                offset = raw.find(b'pssh', offset)
+
+                if offset == -1:
+                    break
+
+                pssh_offset = offset - 4
+                size = int.from_bytes(raw[pssh_offset:offset], byteorder='big')
+                offset += size
+                yield PSSH(raw[pssh_offset:pssh_offset + size])
+
+        init_data = self._downloader.urlopen(Request(
+            part['fragment_base_url'] + part['fragments'][0]['path'],
+            headers=part['http_headers'])).read()
+
+        for pssh in find_wv_pssh_offsets(init_data):
+            if pssh.system_id == PSSH.SystemId.Widevine:
+                self.to_screen(f'Extracted PSSH from init segment')
+                return pssh.dumps()
+
+    def _fetch_keys(self, info, pssh, callback):
+        if keys := self._keys.get(pssh):
+            return keys
+
+        keys = ()
+
+        if devicepath := self._kwargs.get('devicepath'):
+            cdm = Cdm.from_device(Device.load(devicepath))
+            session_id = cdm.open()
+            challenge = cdm.get_license_challenge(session_id, PSSH(pssh), 'STREAMING', privacy_mode=True)
+            cdm.parse_license(session_id, callback(challenge))
+
+            for key in cdm.get_keys(session_id):
+                if key.type == 'CONTENT':
+                    keyarg = '%s:%s' %(key.kid.hex, key.key.hex())
+                    self.to_screen(f'Fetched key: {keyarg}')
+                    keys += ('--key', keyarg)
+
+        self._keys[pssh] = keys
+        return keys
+
+    def _decrypt_part(self, keys, filepath):
+        originalpath = filepath
+
+        # mp4decrypt on Windows cannot handle certain filenames
+        filepath = sub(r'[^0-9A-z_\-.]+', '', filepath)
+        rename(originalpath, filepath)
+
+        tmppath = '_decrypted_' + filepath
+        cmd = ('mp4decrypt',) + keys + (filepath, tmppath)
+
+        _, stderr, returncode = Popen.run(
+            cmd, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+
+        if returncode != 0:
+            raise PostProcessingError(stderr)
+
+        rename(filepath, originalpath)
+        filepath = originalpath
+
+        replace(tmppath, filepath)
